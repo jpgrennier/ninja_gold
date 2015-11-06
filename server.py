03683b535ade9de96273db724a51e716edd2a6b6
Joseph Grennier <jp_grennier@yahoo.com>
2015-11-06 13:18:36 -0800

    building out server, logic and html

--- server.py
+++ server.py
@@ -8,6 +8,25 @@ def index():
 	return render_template('index.html')
 
 @app.route('/process_money' methods=['POST'])
+	try:
+		session['gold']
+	except:
+		session['gold'] = 0
+
+	try: 
+		session['activities']
+	except:
+		session['activities'] = 0
+
+if request.form['whatever'] == 'farm':
+	gold = random.randrange(10,21)
+elif request.form['whatever'] == 'cave':
+	gold = random.randrange(5,11)
+elif request.form['whatever'] == 'house':
+	gold = random.randrange(2,6)
+elif request.form['whatever'] == 'casino':
+	gold = random.randrange(-50,51)
+
 	return redirect('/')
 
 app.run(debug=True)
