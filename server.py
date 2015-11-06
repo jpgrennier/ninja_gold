4e071b601b42d513f7f99c8114f42047bc715afe
Joseph Grennier <jp_grennier@yahoo.com>
2015-11-06 14:19:39 -0800

    gold counter works

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import FLask, session, request, redirect, render_template
+from flask import Flask, session, request, redirect, render_template
 import random
 app = Flask(__name__)
 app.secret_key ='wordsAndStuffAndThings'
@@ -7,7 +7,8 @@ app.secret_key ='wordsAndStuffAndThings'
 def index():
 	return render_template('index.html')
 
-@app.route('/process_money' methods=['POST'])
+@app.route('/process_money', methods=['POST'])
+def process_money():
 	try:
 		session['gold']
 	except:
@@ -18,14 +19,18 @@ def index():
 	except:
 		session['activities'] = 0
 
-if request.form['whatever'] == 'farm':
-	gold = random.randrange(10,21)
-elif request.form['whatever'] == 'cave':
-	gold = random.randrange(5,11)
-elif request.form['whatever'] == 'house':
-	gold = random.randrange(2,6)
-elif request.form['whatever'] == 'casino':
-	gold = random.randrange(-50,51)
+	if request.form['whatever'] == 'farm':
+		gold = random.randrange(10,21)
+		# session['activity] = 'test'
+	elif request.form['whatever'] == 'cave':
+		gold = random.randrange(5,11)
+	elif request.form['whatever'] == 'house':
+		gold = random.randrange(2,6)
+	elif request.form['whatever'] == 'casino':
+		gold = random.randrange(-50,51)
+
+	session['gold'] += gold
+	session['activity'] 
 
 	return redirect('/')
 
